0b297ee6e49ce4a5d0f8182079d72f3ed2c46a23
Mr.Lin <714480119@qq.com>
2023-09-30 19:07:20 +0800

    build script

--- build.bat
+++ build.bat
@@ -18,10 +18,15 @@ for /f "tokens=2 delims==" %%i in ('wmic path win32_operatingsystem get LocalDat
 )
 set "Now=%strDate:~0,4%-%strDate:~4,2%-%strDate:~6,2%,%strDate:~8,2%:%strDate:~10,2%:%strDate:~12,2%"
 
+setlocal EnableDelayedExpansion
+for /f "delims=" %%i in ('git rev-parse HEAD') do (
+  set "commitId=%%i"
+)
+
 set ver=%1
 if [%ver%] == [] (set ver=unkonw)
 @echo on
-go build -ldflags "-X main.buildTime=%Now% -X main.buildVersion=%ver%" -o main
+go build -ldflags "-X main.buildTime=%Now% -X main.buildVersion=%ver% -X main.gitCommitID=%commitId%" -o main
 chmod a+x main
 
 @echo off
